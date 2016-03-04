20628fe490521ec02ecb8e4bf81548eb8fdae8b4
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 11:39:15 +0100

    Initial Commit

---
+++ Reflection/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
